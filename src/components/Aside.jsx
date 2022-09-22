fcd8ef6507f2c28b38f3d7a6c3f96b22d219d8a1
ZSchmitt <zaiane.schmitt@gmail.com>
2022-09-22 18:51:04 +0200

    refactor: center vertical align + height

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Aside = () => {
   return (
-    <aside className="bg-black w-[107px] h-screen flex flex-col items-center">
-      <nav className="ico-aside-container w-1/2 mt-[200%]">
+    // <aside className="bg-black w-[107px] flex flex-col items-center">
+    <aside className="hidden lg:sticky float-left h-screen w-28 bg-black top-0 z-30 flex-col lg:flex items-center">
+      <nav className="ico-aside-container w-1/2 mt-[200%] flex flex-col ml-[auto] mr-[auto]">
         <div className="ico-aside mb-[20px]">
           <img
             src={`${process.env.PUBLIC_URL}/img/ico-aside/icon-zen.svg`}
@@ -29,7 +30,7 @@ const Aside = () => {
           />
         </div>
       </nav>
-      <p className="copyright text-white text-xs -rotate-90 w-[150px] mt-[180%]">
+      <p className="copyright text-white text-xs -rotate-180 ml-[auto] mr-[auto] vertical-writing mt-[180px]">
         Copyright, Sportsee 2020
       </p>
     </aside>
