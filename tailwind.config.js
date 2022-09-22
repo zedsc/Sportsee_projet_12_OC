660369f1e35f2cffca4b275af42534ed844605c5
ZSchmitt <zaiane.schmitt@gmail.com>
2022-09-22 18:53:39 +0200

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        large: "1440px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    ({ addUtilities }) => {
+      addUtilities({
+        ".vertical-writing": {
+          writingMode: "vertical-lr",
+        },
+      });
+    },
+  ],
 };
