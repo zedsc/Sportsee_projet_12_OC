93e87c46dd1e357a196334676b4a376cf7483a4b
ZSchmitt <zaiane.schmitt@gmail.com>
2022-09-23 21:12:47 +0200

    feat: error message component

---
+++ src/components/ErrorMsg.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorMsg = () => {
+  return (
+    <div className="error-api text-center mt-[10%] text-lg">
+      <p>Oups, il y a eu un problème. Service indisponible ❌</p>
+    </div>
+  );
+};
+
+export default ErrorMsg;
