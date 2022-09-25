2e6d299085600d2d9b3549200b9ebea5664c6024
ZSchmitt <zaiane.schmitt@gmail.com>
2022-09-25 17:21:58 +0200

    refactor: state - datas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,21 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import axios from "axios";
 import Header from "../components/Header";
 import Aside from "../components/Aside";
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
-  ////Test de data
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios.get("/user/12.json").then((result) => setData(result.data));
-  }, []);
-
-  console.log(data, "datas");
-
   return (
-    <div className="main-wrapper mx-auto">
+    <div className="main-wrapper mx-auto h-[100%]">
       <Header />
       <div className="wrapper-main-aside flex">
         <Aside />
         <main className="w-[100%]">
-          <div className="user-select flex mt-[150px] justify-center">
-            <NavLink to="/dashboard">
-              <div className="user card w-[400px] h-[420px] mx-[40px] shadow-[0_4px_4px_0_rgba(0,0,0,0.35)] rounded-lg overflow-hidden">
+          <h1 className="pick-user text-2xl mx-auto mt-[80px] w-[300px]">
+            Choisissez un utilisateur :
+          </h1>
+          <div className="user-select flex mt-[100px] justify-center">
+            <NavLink to="/dashboard" state={18}>
+              <div className="user card w-[400px] h-[420px] mx-[40px] shadow-[0_2px_6px_0_rgba(0,0,0,0.35)] rounded-lg overflow-hidden">
                 <img
                   src={`${process.env.PUBLIC_URL}/img/persona_f.jpg`}
                   alt="Utilisateur 1"
@@ -32,8 +23,8 @@ const Home = () => {
                 />
               </div>
             </NavLink>
-            <NavLink to="/dashboard">
-              <div className="user card w-[400px] h-[420px] mx-[40px] shadow-[0_4px_4px_0_rgba(0,0,0,0.35)] rounded-lg overflow-hidden">
+            <NavLink to="/dashboard" state={12}>
+              <div className="user card w-[400px] h-[420px] mx-[40px] shadow-[0_2px_6px_0_rgba(0,0,0,0.35)] rounded-lg overflow-hidden">
                 <img
                   src={`${process.env.PUBLIC_URL}/img/persona_m.jpg`}
                   alt="Utilisateur 2"
